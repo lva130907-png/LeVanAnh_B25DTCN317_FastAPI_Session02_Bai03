906568c5eee42419194d9b1ef0baaa2fd71b4824
Lê Vân Anh <lva130907@gmail.com>
2026-06-28 18:55:20 +0700

    Update bai03.py

--- bai03.py
+++ bai03.py
@@ -1,3 +1,17 @@
+""" 
+1. Input của bài toán là danh sách students
+
+2. Output mong muốn là danh sách sinh viên có status = "active"
+
+3. Điều kiện dùng để xác định sinh viên đang học: student["status"] == "active"
+
+4. Các bước xử lý API GET /students/active:
+    Duyệt danh sách students
+    Lọc các sinh viên có status = "active"
+        Nếu không có, trả về thông báo và mảng rỗng
+        Nếu có, trả về danh sách sinh viên đang học
+"""
+
 from fastapi import FastAPI
 
 app = FastAPI()
